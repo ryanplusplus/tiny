aa5070a77401fb43c374d7ff338229aab4731398
Ryan Hartlage <ryanplusplus@gmail.com>
2019-08-18 14:17:59 -0400

    Added container_of

--- src/tiny_utils.h
+++ src/tiny_utils.h
@@ -6,13 +6,16 @@
 #ifndef tiny_utils_h
 #define tiny_utils_h
 
-#define reinterpret(name, cast_me, type) \
-  type name = (type)cast_me;
+#define reinterpret(_name, _cast_me, _type) \
+  _type _name = (_type)_cast_me;
 
-#define element_count(array) \
-  (sizeof(array) / sizeof(array[0]))
+#define element_count(_array) \
+  (sizeof(_array) / sizeof(_array[0]))
 
-#define element_size(array) \
-  sizeof(array[0])
+#define element_size(_array) \
+  sizeof(_array[0])
+
+#define container_of(_container_type, _field_name, _field) \
+  (_container_type*)((uint8_t*)_field - offsetof(_container_type, _field_name))
 
 #endif
