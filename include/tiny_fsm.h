1a91e1f7dbdddd6d8f4860083a83bf16cc94087d
Ryan Hartlage <ryanplusplus@gmail.com>
2021-04-21 21:10:48 -0400

    Un-inline tiny_fsm_transition

--- include/tiny_fsm.h
+++ include/tiny_fsm.h
@@ -56,11 +56,6 @@ inline void tiny_fsm_send_signal(tiny_fsm_t* self, tiny_fsm_signal_t signal, con
  * Transitions the FSM to the target state. Sends exit to the current state, changes
  * state, then sends entry to the target state.
  */
-inline void tiny_fsm_transition(tiny_fsm_t* self, tiny_fsm_state_t target)
-{
-  self->current(self, tiny_fsm_signal_exit, NULL);
-  self->current = target;
-  self->current(self, tiny_fsm_signal_entry, NULL);
-}
+void tiny_fsm_transition(tiny_fsm_t* self, tiny_fsm_state_t target);
 
 #endif
--- src/tiny_fsm.c
+++ src/tiny_fsm.c
@@ -9,4 +9,9 @@ extern inline void tiny_fsm_init(tiny_fsm_t* self, tiny_fsm_state_t initial);
 
 extern inline void tiny_fsm_send_signal(tiny_fsm_t* self, tiny_fsm_signal_t signal, const void* data);
 
-extern inline void tiny_fsm_transition(tiny_fsm_t* self, tiny_fsm_state_t target);
+void tiny_fsm_transition(tiny_fsm_t* self, tiny_fsm_state_t target)
+{
+  self->current(self, tiny_fsm_signal_exit, NULL);
+  self->current = target;
+  self->current(self, tiny_fsm_signal_entry, NULL);
+}
