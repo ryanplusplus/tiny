4443a3df6ceb4b686960b56c119341050308efef
Ryan Hartlage <ryanplusplus@gmail.com>
2022-01-10 18:13:48 -0500

    Added lib.mk

---
+++ lib_tiny.mk
@@ -0,0 +1,21 @@
+__lib_tiny_path := $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
+
+LIBS := tiny $(LIBS)
+
+INC_DIRS += \
+  lib/tiny/include \
+
+tiny_ASFLAGS := $(ASFLAGS)
+tiny_CPPFLAGS := $(CPPFLAGS)
+tiny_CFLAGS := $(CFLAGS)
+tiny_CXXFLAGS := $(CXXFLAGS)
+
+tiny_INC_DIRS := \
+  $(__lib_tiny_path)include \
+
+tiny_SYS_INC_DIRS := \
+
+tiny_SRC_FILES := \
+
+tiny_SRC_DIRS := \
+  $(__lib_tiny_path)src \
