d13dd2bfe5d5b2fd089fc0ce1841f604b4d0dced
Ryan Hartlage <ryanplusplus@gmail.com>
2022-01-30 16:03:21 -0500

    Debug with cmake build

--- test/asan_options.cpp
+++ test/asan_options.cpp
@@ -5,5 +5,5 @@
 
 extern "C" const char* __asan_default_options()
 {
-  return "new_delete_type_mismatch=0:alloc_dealloc_mismatch=0:abort_on_error=1";
+  return "new_delete_type_mismatch=0:alloc_dealloc_mismatch=0:abort_on_error=1:detect_leaks=0";
 }
